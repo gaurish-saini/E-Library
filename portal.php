c58de6688f40fbf171aeababb0cc85070dee6828
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-04-15 11:57:37 +0530

    portal in-progress

--- portal.php
+++ portal.php
@@ -3,32 +3,35 @@
 include('config/db_connect.php');
 session_start();
 
-$username = $password = $cpassword = '';   
+$username = $password = '';   
 $exists = false;   
-$errors = array('username'=>'','password'=>'');
 
 //LOGIN
 
 if(isset($_POST['login'])){   
 
     
-     if(!array_filter($errors)){
+     if(!empty($_POST['password'] && $_POST['username'])){
         if($_SERVER["REQUEST_METHOD"] == "POST") {
 
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST["password"]); 
       
-      $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword' and role='admin'";
+      $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       
       $count = mysqli_num_rows($result);
       	
       if($count == 1) {
-        $_SESSION['login_user'] = $myusername; 
-         header('location: index.php');
-      }else {
-         $error = "Your Login Name or Password is invalid";
+          if ($row['role'] =='admin'){
+            $_SESSION['login_user'] = $myusername; 
+            header('location: index.php');
+          }
+          if ($row['role'] =='reader'){
+            $_SESSION['login_user'] = $myusername; 
+            header('location: reader.php');
+          } 
       }
    }
 }
@@ -50,29 +53,25 @@ if(isset($_POST['login'])){
                         <div class="input-field indigo-text text-darken-4 ">
                             <input type="text" value="<?php echo htmlspecialchars($username) ?>" id="username" name="username">
                             <label for="email">Username*</label>
-                            <!-- <div class="red-text"><?php echo $errors['username']; ?></div> -->
                         </div>
                         <div class="input-field indigo-text text-darken-4 ">
                             <input type="password" value="<?php echo htmlspecialchars($password) ?>" id="password" name="password">
                             <label for="password">Password*</label>
-                            <!-- <div class="red-text"><?php echo $errors['password']; ?></div> -->
                         </div>
                         <div class="center">
                             <input type="submit" name="login" value="sign in" class="btn indigo-text tab-color z-depth-0 ">
                             <?php 
                             if(isset($_POST['login'])):?>
-                                    <?php if(empty($_POST['password' && 'username'])):?>
+                                    <?php if(empty($_POST['password'] && $_POST['username'])):?>
                                         <script>M.toast({html: 'Invalid Credentials !'})</script>
-                                    <?php else: ?>
-                                                <?php if(empty($_POST['username'])):?>
-                                                    <script>M.toast({html: 'Username Required'})</script>
-                                                <?php elseif(empty($_POST['password'])):?>
-                                                    <script>M.toast({html: 'Password Required'})</script>
-                                                
-                                                <?php endif ?>
-                                    <?php endif ?>            
-                            <?php endif ?>
-                            
+                                        <?php endif ?>
+                                    <?php if(empty($_POST['username'])):?>
+                                        <script>M.toast({html: 'Username Required'})</script>
+                                        <?php endif ?>
+                                    <?php if(empty($_POST['password'])):?>
+                                        <script>M.toast({html: 'Password Required'})</script>
+                                        <?php endif ?>
+                            <?php endif ?>            
                         </div></br>
                         <div><label class="portal-label">New Reader? <a href="register.php" class="label-link">Create a Account</a></label></br></div>
                     </form>
--- register.php
+++ register.php
@@ -3,43 +3,45 @@
 include('config/db_connect.php');
 session_start();
 
-$username = $password = $cpassword = ''; 
-$showAlert = false;  
-$showError = false;  
+$username = $password = $cpassword = '';    
 $exists = false;   
 $errors = array('username'=>'','password'=>'');
 
-if(isset($_POST['rsignup'])) {
+if(isset($_POST['signup'])) {
        
-       $username = $_POST["username"];  
-       $password = $_POST["password"];  
-       $cpassword = $_POST["cpassword"]; 
-               
-       $sql = "Select * from users where username='$username'"; 
-       $result = mysqli_query($conn, $sql);        
-       $num = mysqli_num_rows($result);  
+    if(!empty($_POST['password'] && $_POST['username'])){
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $username =  mysqli_real_escape_string($conn,$_POST['username']);  
+                $password =  mysqli_real_escape_string($conn,$_POST['password']);;  
+                $cpassword =  mysqli_real_escape_string($conn,$_POST['cpassword']);; 
+                        
+                $sql = "SELECT * FROM users WHERE username='$username'"; 
+                $result = mysqli_query($conn, $sql);        
+                $num = mysqli_num_rows($result);  
 
-       if($num == 0) { 
-           if(($password == $cpassword) && $exists==false) { 
-               $sql = "INSERT INTO users(username,password,role) VALUES ('$username','$password','reader')"; 
-               $result = mysqli_query($conn, $sql);
-               if ($result) { 
-                   $showAlert = true;
-                   $_SESSION['login_user'] = $_POST['username'];
-                   header('Location: reader.php');
-               } else {
-                   echo 'query error: '. mysqli_error($conn);
-               }
-           }  
-           else {  
-               $showError = "Passwords do not match";  
-           }       
-       }   
-      if($num>0)  
-      { 
-         $exists="Username not available";  
-      }     
-   }
+                if($num == 0) { 
+                    if(($password == $cpassword) && $exists==false) { 
+                        $sql = "INSERT INTO users(username,password,role) VALUES ('$username','$password','reader')"; 
+                        $result = mysqli_query($conn, $sql);
+                        if ($result) { 
+                            $_SESSION['login_user'] = $_POST['username'];
+                            header('Location: reader.php');
+                        } else {
+                            echo 'query error: '. mysqli_error($conn);
+                        }
+                    }  
+                    else {  
+                        $showError = "Passwords do not match";  
+                    }       
+                }   
+                if($num>0)  
+                { 
+                    $exists="Username not available";  
+                }     
+            }
+
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -51,12 +53,10 @@ if(isset($_POST['rsignup'])) {
 
 <section class="container grey lighten-4 " id="login-box"></section>
     <div class="container col s12 md12">
-        <div class="row">  
+        
 <!-- signup  -->
             <div class="col s6 md6 center">
-                <label style="color: #ee6e73;">New Reader? <label>Create a Account</label></label></br></br></br>
-                <div id="reader">
-                    <form action="register.php" method="POST">
+                    <form class="border" action="register.php" method="POST">
                         <div class="input-field indigo-text text-darken-4 ">
                             <input type="text" value="<?php echo htmlspecialchars($username) ?>" id="username" name="username">
                             <label for= "email">Create Username*</label>
@@ -67,16 +67,30 @@ if(isset($_POST['rsignup'])) {
                         </div>
                         <div class="input-field indigo-text text-darken-4 ">
                             <input type="password" value="<?php echo htmlspecialchars($cpassword) ?>" id="cpassword" name="cpassword">
-                            <label for= "password">Confirm Password*</label></br></br></br>
+                            <label for= "password">Confirm Password*</label></br>
                         </div>
-                            <div class="center border">
-                                <input type="submit" name="rsignup" value="sign up" class="btn indigo-text tab-color z-depth-0">
-                            </div>
-                       
+                        <div class="center">
+                            <input type="submit" name="signup" value="sign up" class="btn indigo-text tab-color z-depth-0">
+                            <?php 
+                            if(isset($_POST['signup'])):?>
+                                    <?php if(empty($_POST['username'] && $_POST['password'] && $_POST['cpassword'])):?>
+                                        <script>M.toast({html: 'Fill out required fields !'})</script>
+                                        <?php endif ?>
+                                    <?php if(empty($_POST['username'])):?>
+                                        <script>M.toast({html: 'Username Required'})</script>
+                                        <?php endif ?>
+                                    <?php if(empty($_POST['password'])):?>
+                                        <script>M.toast({html: 'Password Required'})</script>
+                                        <?php endif ?>
+                                    <?php if(empty($_POST['cpassword'])):?>
+                                        <script>M.toast({html: 'Confirm Password'})</script>
+                                        <?php endif ?>
+                            <?php endif ?>   
+                        </div></br>
+                        <div><label class="portal-label">Already a User? <a href="portal.php" class="label-link">Log In</a></label></br></div>
                     </form>
-                </div>
             </div>
-        </div>
+        
     </div>
 </section>
 	
