c970798b019fc8463acb40488db565fdff081d0f
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-05-03 15:40:02 +0530

    password reset code

--- core/configs/PHPMailer/config.php
+++ core/configs/PHPMailer/config.php
@@ -3,11 +3,11 @@ $mail->SMTPDebug = 0;
 $mail->isSMTP();                                      
 $mail->Host = 'smtp.gmail.com';                    
 $mail->SMTPAuth = true;                            
-$mail->Username   = 'gaurishprakhar@gmail.com';
-$mail->Password   = 'Gaurish@5162';              
+$mail->Username   = '';
+$mail->Password   = '';              
 $mail->SMTPSecure = 'ssl';  
 $mail->Host = 'ssl://smtp.gmail.com:465';
 $mail->Port = 465;   
-$mail->setFrom('gaurishprakhar@gmail.com', 'eLibrary | Do Not Reply');
+$mail->setFrom('', 'eLibrary | Do Not Reply');
 $mail->isHTML(true);           
 ?>
