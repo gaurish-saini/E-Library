320beda9c98515501e14784ebf9cbc1979dbff8d
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-06-03 23:19:59 +0530

    Script Bug fix

--- core/materialize.php
+++ core/materialize.php
@@ -4,8 +4,8 @@ require __dir__.'/'.'models/database/queryBuilder.php';
 require __dir__.'/'.'models/user.php';
 require __dir__.'/'.'models/Mail.php'; 
 require __dir__.'/'.'models/book.php';
-require __dir__.'/Router.php';
-require __dir__.'/Request.php';	
+require __dir__.'/router.php';
+require __dir__.'/request.php';	
 require __dir__.'/../resources/materialize/materialize_js.php';	
 require __dir__.'/../resources/materialize/materialize_css.php'
 ?>
